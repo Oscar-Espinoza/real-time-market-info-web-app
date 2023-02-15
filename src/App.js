fb210f430b4c1ac395d2bd09d2cf0c6cacff3ed2
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-02-15 15:40:09 -0300

    still the same error?¡

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import CurrencyDetails from './components/CurrencyDetails/CurrencyDetails';
-import CurrencyHistory from './components/CurrencyHistory/CurrencyHistory';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:currencyName" element={<CurrencyDetails />} />
-        <Route path="/:currencyName/history" element={<CurrencyHistory />} />
       </Routes>
     </div>
   );
